bc219f5c23e57bb77e52ba8d7807f64683f71222
govind8061 <91026913+govind8061@users.noreply.github.com>
2021-09-20 11:06:33 +0530

    Update urls.py

--- gs1/urls.py
+++ gs1/urls.py
@@ -1,18 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-
 from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.codestack_home),
     path('stuinfo/<int:pk>/', views.student_detail),
-    path('stuinfo/', views.student_list), 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('stuinfo/', views.student_list),
 ]
